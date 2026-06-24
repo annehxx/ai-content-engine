6e05edaf78e3421e74fa6e5faf96c1a862932f60
Anne Heinrich <67550502+annehxx@users.noreply.github.com>
2026-06-24 13:05:25 +0200

    Change spacing

--- engine/generator.py
+++ engine/generator.py
@@ -180,7 +180,7 @@ class ContentGenerator:
         centered_text(
             draw,
             post.subtitle.title(),
-            current_y + 24,
+            current_y + 48,
             subtitle_font,
             (15, 15, 15),
             self.platform.width,
