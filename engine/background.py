48ce106f1346acf27e42cc8bb0db40ba3b1bde6b
Anne Heinrich <67550502+annehxx@users.noreply.github.com>
2026-06-24 12:37:32 +0200

    Remove decorative circles from background

--- engine/background.py
+++ engine/background.py
@@ -10,8 +10,6 @@ from config import BACKGROUND_FILE_CANDIDATES, BACKGROUNDS_DIR
 def make_background(width: int, height: int, color: tuple[int, int, int]) -> Image.Image:
     image = Image.new("RGBA", (width, height), color + (255,))
     draw = ImageDraw.Draw(image)
-    draw.ellipse((-160, -120, 300, 280), fill=(255, 255, 255, 60))
-    draw.ellipse((760, 1010, 1220, 1490), fill=(255, 255, 255, 70))
     draw.rounded_rectangle((70, 100, width - 70, height - 100), radius=42, outline=(255, 255, 255, 45), width=3)
     return image
 
